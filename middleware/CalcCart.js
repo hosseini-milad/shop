e21cce8fc622f1b7b98f9628c62a4e420b84f842
milad <milad.cia@gmail.com>
2024-01-16 00:12:02 +0330

    cart

--- middleware/CalcCart.js
+++ middleware/CalcCart.js
@@ -8,9 +8,9 @@ const CalcCart=async(cartDetails)=>{
     var totalPrice = 0
     var totalCount = 0
     for(var c=0;c<cartDetails.length;c++){
-    const ItemId = cartDetails[c].ItemId
+    //const ItemId = 
     const priceData = await productPrice.findOne(
-        {ItemID:ItemId.ItemID,saleType:SaleType},
+        {ItemID:cartDetails[c].ItemId,saleType:SaleType},
         {price:1,_id:0})
     
     cartDetails[c].price=NormalTax(priceData.price)
