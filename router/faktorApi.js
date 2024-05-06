962557fe840feccdc3eed9b9cda1c5cb478d0a24
milad <milad.cia@gmail.com>
2024-05-06 18:44:25 +0330

    no cart

--- router/faktorApi.js
+++ router/faktorApi.js
@@ -279,14 +279,13 @@ const findCartFunction=async(userId,managerId)=>{
             const userData = await customers.findOne({_id:ObjectID(cartData[c].userId)})
             cartData[c] = {...cartData[c],userData:userData}
             cartDetail.push(findCartSum(cartData[c].cartItems))
-
-    
+        }
+    catch{}
         
         if(qCartData) {
             qCartDetail =findQuickCartSum(qCartData.cartItems,
             qCartData.payValue,qCartData.discount)
-        }}
-    catch{}
+        }
    }
     return({cart:cartData,cartDetail:cartDetail,userData:"userData",
         quickCart:qCartData,qCartDetail:qCartDetail})
