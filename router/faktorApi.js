c40f83f98b0459cdd02a535f683e49b66321f818
milad <milad.cia@gmail.com>
2024-05-13 13:44:42 +0330

    cart list

--- router/faktorApi.js
+++ router/faktorApi.js
@@ -264,7 +264,7 @@ const findCartFunction=async(userId,managerId)=>{
     const qCartData = await qCart.findOne({userId:userId})
     const qCartAdmin = await qCart.aggregate([
         {$match:{manageId:managerId}},
-        {$math:{cartItems:{$nin:[[],'']}}},
+        {$math:{cartItems:{$ne:[]}}},
         { $addFields: { "userId": { "$toObjectId": "$userId" }}},
             {$lookup:{
                 from : "customers", 
