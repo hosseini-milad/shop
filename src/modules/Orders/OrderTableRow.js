fda98e0da8d44a3c15a479aae178eb9ddf6c7f77
sinaamini92 <sinaaminioffice@gmail.com>
2024-05-25 16:38:30 +0330

    user's profile

--- src/modules/Orders/OrderTableRow.js
+++ src/modules/Orders/OrderTableRow.js
@@ -1,116 +1,189 @@
-import React ,{ useState } from "react"
-import Status from "../Components/Status"
-import PayStatus from "../Components/PayStatus"
-import  { normalPriceCount, rxFindCount } from "../../env"
-import OrderQuickDetail from "./OrderComponent/OrderQuickDetail"
-import tabletrans from "../../translate/tables"
-import OrderQuickCart from "./OrderComponent/OrderQuickCart"
+import React, { useState } from "react";
+import Status from "../Components/Status";
+import PayStatus from "../Components/PayStatus";
+import { normalPriceCount, rxFindCount } from "../../env";
+import OrderQuickDetail from "./OrderComponent/OrderQuickDetail";
+import tabletrans from "../../translate/tables";
+import OrderQuickCart from "./OrderComponent/OrderQuickCart";
 
+function OrderTableRow(props) {
+  const [openOption, setOpenOption] = useState(0);
+  const [checkState, setCheckState] = useState(false);
+  const activeAcc = props.index === props.detail;
+  const order = props.order;
+  const lang = props.lang;
+  const cart = props.cart;
+  console.log(order);
+  return (
+    <React.Fragment>
+      <tr className={activeAcc ? "activeAccordion" : "accordion"}>
+        <td className="checkBoxStyle">
+          <input
+            type="checkbox"
+            name=""
+            id=""
+            checked={checkState}
+            onChange={(e) => setCheckState(checkState ? false : true)}
+          />
+        </td>
+        <td>
+          <div className="order-id">
+            {cart ? (
+              <p
+                onClick={() =>
+                  (window.location.href = "/orders/detail/" + order.cartNo)
+                }
+              >
+                {order.cartNo}
+              </p>
+            ) : (
+              <p
+                onClick={() =>
+                  (window.location.href = "/orders/detail/" + order.orderNo)
+                }
+              >
+                {order.orderNo}
+              </p>
+            )}
+          </div>
+        </td>
+        <td>
+          <div className="cu-avatar">
+            <img src="/img/avatar/avatar_1.jpg" alt="avatar" />
+            <div className="cu-name">
+              <p className="name">
+                {order.userInfo[0]
+                  ? order.userInfo[0].cName + "---" + order.userInfo[0].sName
+                  : "---"}
+              </p>
+            </div>
+            {order.moreInformation ? (
+              <i className="fa fa-comment-o" title={order.moreInformation}></i>
+            ) : (
+              <></>
+            )}
+          </div>
+        </td>
+        <td>
+          <div className="order-num">
+            <p className="email">
+              {order.userInfo[0]
+                ? order.userInfo[0].phone
+                : tabletrans.notEntered[lang]}
+            </p>
+          </div>
+        </td>
+        <td>
+          <PayStatus
+            payStatus={order.payStatus}
+            class={"order-status"}
+            lang={props.lang}
+          />
+        </td>
+        <td>
+          <div className="order-num">
+            <p>{order.transport}</p>
+          </div>
+        </td>
+        <td>
+          {cart ? (
+            <div className="or-date">
+              <p className="date">
+                {new Date(order.initDate).toLocaleDateString(
+                  props.lang === "persian" ? "fa" : "en"
+                )}
+              </p>
+              <p className="time">
+                {new Date(order.initDate).toLocaleTimeString(
+                  props.lang === "persian" ? "fa" : "en"
+                )}
+              </p>
+            </div>
+          ) : (
+            <div className="or-date">
+              <p className="date">
+                {new Date(order.date).toLocaleDateString(
+                  props.lang === "persian" ? "fa" : "en"
+                )}
+              </p>
+              <p className="time">
+                {new Date(order.date).toLocaleTimeString(
+                  props.lang === "persian" ? "fa" : "en"
+                )}
+              </p>
+            </div>
+          )}
+        </td>
 
-function OrderTableRow(props){
-  const [openOption,setOpenOption] = useState(0)
-  const [checkState,setCheckState] = useState(false)
-  const activeAcc = props.index===props.detail
-  const order=props.order
-  const lang=props.lang;
-  const cart = props.cart
-console.log(order);
-    return(<React.Fragment>
-        <tr 
-            className={activeAcc?"activeAccordion":"accordion"}>
-            <td className="checkBoxStyle">
-              <input type="checkbox" name="" id="" checked={checkState}
-              onChange={(e)=>setCheckState(checkState?false:true)}/></td>
-            <td>
-                <div className="order-id">
-                  {cart?<p onClick={()=> window.location.href=
-                    "/orders/detail/"+order.cartNo}>
-                    {order.cartNo}</p>:
-                    <p onClick={()=> window.location.href=
-                      "/orders/detail/"+order.orderNo}>
-                      {order.orderNo}</p>}
-                </div>
-            </td>
-            <td>
-              <div className="cu-avatar">
-                  <img src="/img/avatar/avatar_1.jpg" alt="avatar"/>
-                  <div className="cu-name">
-                    <p className="name">{order.userInfo[0]?order.userInfo[0].cName+"---"+
-                    order.userInfo[0].sName:'---'}</p>
-                  </div>
-                  {order.moreInformation?
-                    <i className="fa fa-comment-o" title={order.moreInformation}></i>:<></>}
-                </div>
-              </td>
-              <td>
-                <div className="order-num">
-                <p className="email">{order.userInfo[0]?order.userInfo[0].phone:tabletrans.notEntered[lang]}</p>
-                </div>
-              </td>
-              <td>
-                <PayStatus payStatus={order.payStatus} class={"order-status"} 
-                  lang={props.lang}/>
-              </td>
-              <td>
-                <div className="order-num">
-                  <p>{order.transport}</p>
-                </div>
-              </td>
-              <td>
-              {cart?<div className="or-date">
-                  <p className="date">{new Date(order.initDate)
-                  .toLocaleDateString(props.lang==="persian"?'fa':'en')}</p>
-                  <p className="time">{new Date(order.initDate)
-                  .toLocaleTimeString(props.lang==="persian"?'fa':'en')}</p>
-                </div>:
-                <div className="or-date">
-                  <p className="date">{new Date(order.date)
-                  .toLocaleDateString(props.lang==="persian"?'fa':'en')}</p>
-                  <p className="time">{new Date(order.date)
-                  .toLocaleTimeString(props.lang==="persian"?'fa':'en')}</p>
-                </div>
-                  }
-              </td>
+        <td>
+          <div className="order-price">
+            <p>{normalPriceCount(order.totalCart.totalPrice)}</p>
+          </div>
+        </td>
+        <td>
+          <Status
+            status={order.status}
+            class={"order-status"}
+            lang={props.lang}
+          />
+        </td>
 
-
-              <td>
-                <div className="order-price">
-                  <p>{normalPriceCount(order.totalCart.totalPrice)}</p>
-                </div>
-              </td>
-              <td>
-                <Status status={order.status} class={"order-status"} 
-                  lang={props.lang}/>
-              </td>
-
-            <td>
-              <div className="more-btn">
-              <i className={`tableIcon fas ${activeAcc?"fa-chevron-up":"fa-chevron-down"}`} 
-                onClick={()=>props.showDetail(activeAcc?"-1":props.index)} ></i>
-                <i className="tableIcon fas fa-edit" onClick={()=>
-                  window.location.href="/orders/detail/"+order.rxOrderNo}></i>
-                {/* <i className="tableIcon fas fa-ellipsis-v" 
+        <td>
+          <div className="more-btn">
+            <i
+              className={`tableIcon fas ${
+                activeAcc ? "fa-chevron-up" : "fa-chevron-down"
+              }`}
+              onClick={() => props.showDetail(activeAcc ? "-1" : props.index)}
+            ></i>
+            <i
+              className="tableIcon fas fa-edit"
+              onClick={() =>
+                (window.location.href = "/orders/detail/" + order.rxOrderNo)
+              }
+            ></i>
+            <i
+              className="tableIcon fas fa-print"
+              onClick={() =>
+                (window.location.href = "/orders/print/" + order.rxOrderNo)
+              }
+            ></i>
+            {/* <i className="tableIcon fas fa-ellipsis-v" 
                   onClick={()=>setOpenOption(openOption?0:1)}></i> */}
+          </div>
+          {openOption ? (
+            <div className="sub-more-menu">
+              <div className="sub-option sub-delete">
+                <i
+                  className="tableIcon fas fa-remove"
+                  style={{ color: "#ff0000" }}
+                ></i>
+                <p>Delete</p>
               </div>
-              {openOption?<div className="sub-more-menu">
-                <div className="sub-option sub-delete">
-                <i className="tableIcon fas fa-remove" style={{color: "#ff0000"}}></i>
-                  <p>Delete</p>
-                </div>
-                <div className="sub-option sub-edit">
-                  <i className="tableIcon fas fa-edit"></i>
-                  <p>Edit</p>
-                </div>
-              </div>:<></>}
-            </td>
-          </tr>
-          {activeAcc?<tr className="sub-order">
-        <td colSpan="10">{order.orderItems?<OrderQuickDetail 
-          order={order.orderItems}/>:
-          <OrderQuickCart order={order.cartItems}/>}
-          </td></tr>
-          :<React.Fragment></React.Fragment>}
-          </React.Fragment>
-    )
+              <div className="sub-option sub-edit">
+                <i className="tableIcon fas fa-edit"></i>
+                <p>Edit</p>
+              </div>
+            </div>
+          ) : (
+            <></>
+          )}
+        </td>
+      </tr>
+      {activeAcc ? (
+        <tr className="sub-order">
+          <td colSpan="10">
+            {order.orderItems ? (
+              <OrderQuickDetail order={order.orderItems} />
+            ) : (
+              <OrderQuickCart order={order.cartItems} />
+            )}
+          </td>
+        </tr>
+      ) : (
+        <React.Fragment></React.Fragment>
+      )}
+    </React.Fragment>
+  );
 }
-export default OrderTableRow
+export default OrderTableRow;
