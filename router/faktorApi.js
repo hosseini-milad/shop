ce6963d264b91ec95b9b407a1878b1f5161aad33
milad.cia <milad.cia@gmail.com>
2024-07-21 09:38:38 +0330

    capital

--- router/faktorApi.js
+++ router/faktorApi.js
@@ -152,7 +152,7 @@ router.post('/find-products',auth, async (req,res)=>{
             var desc = ''
             var cartCount = 0&&findCartCount(searchProducts[i].sku,currentCart.concat(qCartList),stockId)
             if(count)count.quantity = parseInt(count.quantity)-parseInt(cartCount)
-            if((count&&(count.quantity>0))||count3){
+            if((count&&(count.quantity>0))||(count3&&(count3.quantity>0))){
                 index++
                 desc=searchProducts[i].title+
                 "("+searchProducts[i].sku+")"+
