e5b4a27497678ec7441fb04817f796947447fb94
milad.cia <milad.cia@gmail.com>
2024-10-08 11:32:14 +0330

    excel

--- router/faktorApi.js
+++ router/faktorApi.js
@@ -199,7 +199,8 @@ router.post('/calc-count',auth, async (req,res)=>{
             as : "countData"
         }}
     ])
-    var today = new Date(new Date().toISOString().slice(0, 10)+" 00:00")
+    var date = new Date.now()
+    var today = new Date(date.toISOString().slice(0, 10)+" 00:00")
         const cartList = await tasks.aggregate([
             {$match:{taskStep:{$nin:
             allOrder?['cancel']:['archive','cancel']}}},
