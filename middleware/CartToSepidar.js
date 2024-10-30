d011ce7ac6c295394d2613e5dac08d2ccf01510b
milad.cia <milad.cia@gmail.com>
2024-10-30 08:38:21 +0330

    limit 50

--- middleware/CartToSepidar.js
+++ middleware/CartToSepidar.js
@@ -21,7 +21,7 @@ const CartToSepidar=async(data,faktorNo,user,stock,cartOff)=>{
             notNullCartItem.map((item,i)=>{
                 const price = findPayValuePrice(item.price,3)
                 const itemDiscount = MultiplySum(item.discount,totalOff,1)
-                const discount =item.discount?normalPriceFix(price,itemDiscount)/100:0
+                const discount =itemDiscount?normalPriceFix(price,itemDiscount)/100:0
                 return({
                 "ItemRef": toInt(item.id),
                 "TracingRef": null,
@@ -31,7 +31,7 @@ const CartToSepidar=async(data,faktorNo,user,stock,cartOff)=>{
                 "Fee": toInt(price),
                 "Price": normalPriceCount(price,item.count,1),
                 "Discount": discount?normalPriceFix(discount,item.count):0.0000,
-                "Tax": normalPriceFixCount(price-discount,item.count,TaxRate),
+                "Tax": normalPriceFix(price-discount,item.count,TaxRate),
                 "Duty": 0.0000,
                 "Addition": 0.0000
               })})
@@ -59,17 +59,7 @@ const normalPriceCount=(priceText,count,tax)=>{
       (rawPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").replace( /^\D+/g, ''))
     )
   }
-  const normalPriceFix=(priceText,count)=>{
-    if(!priceText||priceText === null||priceText === undefined) return("")
-    var rawCount = parseFloat(count.toString())
-    var purePrice = priceText.toString().split('.')[0]
-    var rawPrice = (parseInt(purePrice.replace( /,/g, '')
-        .replace(/\D/g,''))*rawCount)
-    return(
-      (rawPrice).toString().split('.')[0]
-    )
-  }
-  const normalPriceFixCount=(priceText,count,mult)=>{
+  const normalPriceFix=(priceText,count,mult)=>{
     if(!priceText||priceText === null||priceText === undefined) return("")
     var rawCount = parseFloat(count.toString())
     var rawMult = mult?parseFloat(mult.toString()):1
