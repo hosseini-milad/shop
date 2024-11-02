972cfcbf63f3bb6f41b776b6e08a0df19f21a215
milad.cia <milad.cia@gmail.com>
2024-11-02 19:28:45 +0330

    quote check

--- router/quoteApi.js
+++ router/quoteApi.js
@@ -734,7 +734,7 @@ router.post('/cart-find', async (req, res) => {
         var canEdit = 0
         var taskData = await OrderToTask(cartData.cartNo)
         if (taskData && (
-            taskData.taskStep == "initial" || taskData.taskStep == "edit"))
+            taskData.taskStep == "initial" || taskData.taskStep == "edit"|| taskData.taskStep == "quote"))
             canEdit = 1
 
         if (!cartData) {
