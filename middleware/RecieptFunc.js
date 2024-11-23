99a7286298412788cd07f38c03b32c867bf15225
milad.cia <milad.cia@gmail.com>
2024-11-23 15:34:31 +0330

    faktor

---
+++ middleware/RecieptFunc.js
@@ -0,0 +1,20 @@
+const RecieptFunc=async(data,FaktorInfo,faktorNo)=>{
+    var query ={
+        "GUID": "124ab075-fc79-417f-b8cf-2a"+faktorNo,
+        "InvoiceID": (FaktorInfo.InvoiceID),
+        "Description": (FaktorInfo.Number),
+        "Date":new Date(),
+        "Drafts": 
+          data.filter(n => n).map((pay,i)=>(
+            {
+            "BankAccountID": pay.bankCode,
+            "Description": pay.title,
+            "Number": pay.description?pay.description:"000",
+            "Date":new Date(),
+            "Amount": pay.payValue
+          }))
+        
+      }
+    return(query)
+}
+module.exports =RecieptFunc
