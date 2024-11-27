c76cefbb1d4cf629512c3705de75b78db44ad189
milad.cia <milad.cia@gmail.com>
2024-11-27 15:01:52 +0330

    manual changes

--- middleware/CurrentExist.js
+++ middleware/CurrentExist.js
@@ -1,15 +1,15 @@
 const tasks = require("../models/crm/tasks")
 const cart = require("../models/product/cart")
 
-const FindCurrentExist=async(itemId,cartNo)=>{
+const FindCurrentExist=async(itemId,cartNo,stockNo)=>{
     const currentTasks = await tasks.find({taskStep:{$nin:["cancel","archive","quote"]}})
-    
+    var stockId=stockNo?stockNo:"13"
     //var validOrder = []
     var countOrder =0
     for(var i=0;i<currentTasks.length;i++){
         if(currentTasks.isQuote) continue;
         if(currentTasks[i].orderNo == cartNo) continue
-        const orderData = await cart.findOne({cartNo:currentTasks[i].orderNo})
+        const orderData = await cart.findOne({cartNo:currentTasks[i].orderNo,stockId:stockId})
         const cartItems = orderData&&orderData.cartItems
         for(var j=0;j<(cartItems&&cartItems.length);j++){
             if(cartItems[j].id === itemId){
