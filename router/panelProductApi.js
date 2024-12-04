30b5013f191a57e4af0a6eeba54d58b2de0cf8ab
milad.cia <milad.cia@gmail.com>
2024-12-04 12:00:39 +0330

    count

--- router/panelProductApi.js
+++ router/panelProductApi.js
@@ -758,6 +758,7 @@ router.post('/report-total',jsonParser,auth,async(req,res)=>{
                 resultData= await UpdateMarket(marketData,manageId,myItem.count,
                     myItem.totalPrice,brandData,cartItems[j].product)
                 marketData = resultData.marketArray
+                myItem.orderList=[reportList[i].cartNo]
                 brandData = resultData.brandArray
                 var index = productList.findIndex(item=>item.sku==myItem.sku)
                 if(index == -1){
@@ -767,11 +768,7 @@ router.post('/report-total',jsonParser,auth,async(req,res)=>{
                     var cNumber = parseInt(productList[index].count)
                     cNumber += parseInt(myItem.count)
                     productList[index].count = cNumber
-                    if(productList[index].orderList){
-                        productList[index].orderList.push(reportList[i].cartNo)
-                    }
-                    else
-                        productList[index].orderList=[reportList[i].cartNo]
+                    productList[index].orderList.push(reportList[i].cartNo)
                     
                     var cPrice = parseInt(productList[index].totalPrice)
                     cPrice += parseInt(myItem.totalPrice)
