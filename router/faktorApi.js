434c277a2ff4f48a4c62f3f3299f1f68b1bae027
milad.cia <milad.cia@gmail.com>
2024-12-07 16:04:25 +0330

    not deactive

--- router/faktorApi.js
+++ router/faktorApi.js
@@ -157,7 +157,7 @@ router.get('/list-filters-panel',auth, async (req, res) => {
         return
     }
     
-    const customerData = await customers.findOne({CustomerID:user.CustomerID})
+    const customerData = await customers.findOne({agent:user.CustomerID})
     try {
         const brandData = await brand.find()
         const catData = await category.aggregate([{
