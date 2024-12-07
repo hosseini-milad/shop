a37be807cd894525628f7461636fcf250f8d4458
milad.cia <milad.cia@gmail.com>
2024-12-07 17:53:02 +0330

    not deactive

--- middleware/FindRemainBank.js
+++ middleware/FindRemainBank.js
@@ -2,7 +2,7 @@ const users = require("../models/auth/users")
 const profiles = require("../models/auth/ProfileAccess")
 var ObjectID = require('mongodb').ObjectID;
 
-const FindRemainBank=async(transactions,total)=>{
+const FindRemainBank=(transactions,total)=>{
     var remain = 0
     var totalPay = total
     /*for(var i=0;i<transactions&&transactions.length;i++){
