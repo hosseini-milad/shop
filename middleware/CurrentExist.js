bcec4269bd87bdfb0a006222e5c58fd43f578d51
milad.cia <milad.cia@gmail.com>
2024-12-17 19:55:27 +0330

    quantity

--- middleware/CurrentExist.js
+++ middleware/CurrentExist.js
@@ -4,15 +4,16 @@ const cart = require("../models/product/cart")
 const FindCurrentExist=async(itemId,cartNo,stockNo)=>{
     const currentTasks = await tasks.find({taskStep:{$nin:["cancel","archive","quote"]}})
     var stockId=stockNo?stockNo:"13"
-    console.log(currentTasks.length)
+        console.log("itemId: ",itemId)
     var countOrder =0
     for(var i=0;i<currentTasks.length;i++){
         if(currentTasks.isQuote) continue;
         if(currentTasks[i].orderNo == cartNo) continue
         const orderData = await cart.findOne({cartNo:currentTasks[i].orderNo,stockId:stockId})
         const cartItems = orderData&&orderData.cartItems
-        console.log(currentTasks[i].orderNo)
         for(var j=0;j<(cartItems&&cartItems.length);j++){
+            
+            console.log("count: ",cartItems[j].count)
             if(cartItems[j].id === itemId){
                 //validOrder.push(cartItems[j]) 
                 countOrder+=parseInt(cartItems[j].count)
