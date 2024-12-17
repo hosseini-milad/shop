552b5cc993596b8ff44112f6affd941cdd7ceed2
milad.cia <milad.cia@gmail.com>
2024-12-17 20:07:33 +0330

    quantity

--- middleware/CurrentExist.js
+++ middleware/CurrentExist.js
@@ -12,7 +12,7 @@ const FindCurrentExist=async(itemId,cartNo,stockNo)=>{
         var cartItems = []
         if(orderData) cartItems = orderData.cartItems
         if(!cartItems) continue
-        console.log("orderNo: ",currentTasks[i].orderNo)
+        console.log("orderNo: ",orderData)
         console.log("stock: ",stockId)
         for(var j=0;j<cartItems.length;j++){
             
