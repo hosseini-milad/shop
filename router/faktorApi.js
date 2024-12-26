807db472b4edb54e255c80464c4cf359537720ca
milad <milad.cia@gmail.com>
2024-12-26 09:08:40 +0330

    changes

--- router/faktorApi.js
+++ router/faktorApi.js
@@ -1683,7 +1683,7 @@ router.post('/cancel-faktor', auth, jsonParser, async (req, res) => {
 const checkCart = async (cartItems, stockId, payValue) => {
     const cartList = await tasks.find({ taskStep: { $nin: ['archive'] } })
     var currentCart = await FindCurrentCart(cartList.map(item => item.orderNo))
-
+    if(!cartItems) return
     const qCartList = await qCart.find(stockId ? { stockId: stockId } : {})
     var checkCart = ''
     for (var i = 0; i < cartItems.length; i++) {
