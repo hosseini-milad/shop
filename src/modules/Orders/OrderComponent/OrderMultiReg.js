9b10b8f049143ef2988379782743af0cbf9d5f40
milad <milad.cia@gmail.com>
2025-01-02 13:38:26 +0330

    reload

--- src/modules/Orders/OrderComponent/OrderMultiReg.js
+++ src/modules/Orders/OrderComponent/OrderMultiReg.js
@@ -50,8 +50,9 @@ function OrderMultiReg(props) {
             );
           }
           else{
+            console.log("123")
             setLoader(0);
-          setTimeout(() => window.location.reload(), 1000);
+          setTimeout(() => window.location.reload(), 100000);
           }
         },
         (error) => {
