854ab54fd5801fc844ff77571af87c3896469bee
milad <milad.cia@gmail.com>
2025-01-11 09:21:47 +0330

    length

--- router/panelProductApi.js
+++ router/panelProductApi.js
@@ -190,6 +190,7 @@ router.post('/list-product',jsonParser,async (req,res)=>{
     var offset = req.body.offset?(parseInt(req.body.offset)):0;
     var userData = await users.findOne({_id:ObjectID(req.headers['userid'])})
     var stockId = req.body.store?req.body.store:userData.StockId
+    var myStock = userData.StoreArr&&userData.StockArr.map(item=>item.StockID)
     try{const data={
         category:req.body.category,
         title:req.body.title,
@@ -254,7 +255,7 @@ router.post('/list-product',jsonParser,async (req,res)=>{
             const typeUnique = [...new Set(productList.map((item) => item.brand))];
             const brandList = await BrandSchema.find()
             const stockList = userData.access=="manager"?
-                await Stocks.find():await Stocks.find({StockID:stockId})
+                await Stocks.find():await Stocks.find({StockID:{$in:myStock}})
            res.json({filter:productList,brands:brandList,
             size:newProduct.length,exists:data.exists,
             quantity:quantity,price:price,stockId,stockList})
