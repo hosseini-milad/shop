a8d1b2c87842169d937439715c395a55506e5bca
milad.cia <milad.cia@gmail.com>
2025-01-20 20:30:49 +0330

    last changes

--- middleware/FindAccess.js
+++ middleware/FindAccess.js
@@ -9,7 +9,7 @@ const FindAccess=async(user)=>{
     if(!profileData||!profileData.length) return([])
     var profileAccess=[]
     for(var i=0;i<profileData.length;i++){
-        profileAccess.push(profileData[i].access)
+        profileAccess.concat(profileData[i].access)
     }
     return(profileAccess)
 }
