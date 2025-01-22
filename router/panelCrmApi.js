904c0011758dec5f3897df2971b3d7ff75361e90
milad.cia <milad.cia@gmail.com>
2025-01-22 15:34:37 +0330

    markazi

--- router/panelCrmApi.js
+++ router/panelCrmApi.js
@@ -253,7 +253,7 @@ router.post('/update-tasks-status', auth, jsonParser, async (req, res) => {
         })
     }
     catch (error) {
-        res.status(500).json({ message: error.message })
+        res.status(500).json({ error: error.message })
     }
 })
 router.post('/update-bulk', auth, jsonParser, async (req, res) => {
