cb722e45e018c4d93d303b11bd05030d9f1d62dc
milad <milad.cia@gmail.com>
2025-01-23 11:43:41 +0330

    similar

--- middleware/FindProfile.js
+++ middleware/FindProfile.js
@@ -7,7 +7,8 @@ const FindProfile=async(access,profile)=>{
     
     var adminProfiles = access?access.map(item=>ObjectID(item)):[]
     const profileData = await ProfileAccess.find({_id: {$in:adminProfiles}})
-    console.log(profileData)
+    console.log(profileData&&profileData.map(item=>
+        item.profileCode),profile)
     if(!profileData||!profileData.length) return(0)
     
     for(var i=0;i<profileData.length;i++){
