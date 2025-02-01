a65f741f755f44a98082fd0de29e6c1de200943f
Amir Hossein Salimi <ah.salimi1431@gmail.com>
2025-02-01 20:20:34 +0330

    debug

--- src/modules/Adv/AdvTable.jsx
+++ src/modules/Adv/AdvTable.jsx
@@ -52,6 +52,48 @@ function AdvTable(props) {
             </div>
           </td>
         </tr>
+        <tr className="accordion">
+          <td>
+            <div>
+              <p className="name bold-fonts">اسلایدر سایت 1</p>
+            </div>
+          </td>
+          <td>اسلایدر صفحه اصلی سایت بالای صفحه</td>
+          <td>
+            <Status status={"true"} class={"order-status"} lang={props.lang} />
+          </td>
+          <td>
+            <div className="more-btn">
+              <i
+                className="tableIcon fas fa-edit"
+                onClick={() =>
+                  (window.location.href = "/advertisment/detail/" + "new")
+                }
+              ></i>
+            </div>
+          </td>
+        </tr>
+        <tr className="accordion">
+          <td>
+            <div>
+              <p className="name bold-fonts">اسلایدر سایت 2</p>
+            </div>
+          </td>
+          <td>اسلایدر صفحه محصولات سایت بالای صفحه</td>
+          <td>
+            <Status status={"true"} class={"order-status"} lang={props.lang} />
+          </td>
+          <td>
+            <div className="more-btn">
+              <i
+                className="tableIcon fas fa-edit"
+                onClick={() =>
+                  (window.location.href = "/advertisment/detail/" + "new")
+                }
+              ></i>
+            </div>
+          </td>
+        </tr>
       </tbody>
     </table>
   );
