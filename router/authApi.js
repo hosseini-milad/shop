44f6067eb0e7fa79c0a0875e87285ae18ef6b156
milad <milad.cia@gmail.com>
2025-02-01 20:03:48 +0330

    crm

--- router/authApi.js
+++ router/authApi.js
@@ -71,7 +71,7 @@ router.post('/login', jsonParser, async (req, res) => {
       );
       user.token = token;
       user.isVisitor = (profile&&profile.includes('visitor'))?1:0
-      user.profileCode = '' //profile.profileCode
+      user.profileCode = profileList
       user.profileName =profile
       res.status(200).json(user);
       return;
