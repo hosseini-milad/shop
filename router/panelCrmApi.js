76017041c1b494d7b27ac0676ab782f0c8993573
Mehrdad Shabanzadeh <mehrdad.sh93@gmail.com>
2025-02-27 20:37:07 +0330

    added try catch

--- router/panelCrmApi.js
+++ router/panelCrmApi.js
@@ -47,150 +47,154 @@ router.post('/fetch-tasks', auth, jsonParser, async (req, res) => {
 });
 
 const calcTasks = async (userId) => {
-	const userData = await user.findOne({ _id: ObjectID(userId) });
-	var access = 3;
-	if (userData.access === 'manager') access = 10;
-	if (userData.access === 'admin') access = 7;
-	if (userData.access === 'client') access = 3;
-	const userAccess = await FindAccess(userData.profile);
-	const allow = userAccess.find((item) => item.title && item.title.includes('Set Order'));
-	if (!allow && access !== 10) {
-		//return({error:"دسترسی ندارد"})
-	}
+	try {
+		const userData = await user.findOne({ _id: ObjectID(userId) });
+		var access = 3;
+		if (userData.access === 'manager') access = 10;
+		if (userData.access === 'admin') access = 7;
+		if (userData.access === 'client') access = 3;
+		const userAccess = await FindAccess(userData.profile);
+		const allow = userAccess.find((item) => item.title && item.title.includes('Set Order'));
+		if (!allow && access !== 10) {
+			//return({error:"دسترسی ندارد"})
+		}
 
-	const crmData = await crmlist.findOne();
-	const crmId = crmData && crmData._id.toString();
+		const crmData = await crmlist.findOne();
+		const crmId = crmData && crmData._id.toString();
 
-	var myCreator = [];
-	if (access == 7) {
-		var userList = await user.find({ profile: { $in: userData.profile } }); //{StockId:userData.StockId})
-		myCreator = userList.map((item) => item._id.toString());
-	}
-	if (access == 3) {
-		myCreator = [userId];
-	}
-	var taskList = await tasks.aggregate([
-		//{$match:limitTask?{profile:limitTask}:{}},
-		{ $match: { crmId: crmId } },
-		{ $match: access < 8 ? { creator: { $in: myCreator } } : {} },
-		{
-			$addFields: {
-				user_Id: {
-					$convert: {
-						input: '$assign',
-						to: 'objectId',
-						onError: '',
-						onNull: '',
-					},
-				},
-			},
-		},
-		{
-			$lookup: {
-				from: 'users',
-				localField: 'user_Id',
-				foreignField: '_id',
-				as: 'userInfo',
-			},
-		},
-		{
-			$addFields: {
-				profile_Id: {
-					$convert: {
-						input: '$profile',
-						to: 'objectId',
-						onError: '',
-						onNull: '',
-					},
-				},
-			},
-		},
-		{
-			$lookup: {
-				from: 'profiles',
-				localField: 'profile_Id',
-				foreignField: '_id',
-				as: 'profileInfo',
-			},
-		},
-		{
-			$addFields: {
-				creator_Id: {
-					$convert: {
-						input: '$creator',
-						to: 'objectId',
-						onError: '',
-						onNull: '',
-					},
-				},
-			},
-		},
-		{
-			$lookup: {
-				from: 'users',
-				localField: 'creator_Id',
-				foreignField: '_id',
-				as: 'creatorInfo',
-			},
-		},
-		{
-			$addFields: {
-				customer_Id: {
-					$convert: {
-						input: '$customer',
-						to: 'objectId',
-						onError: '',
-						onNull: '',
-					},
-				},
-			},
-		},
-		{
-			$lookup: {
-				from: 'customers',
-				localField: 'customer_Id',
-				foreignField: '_id',
-				as: 'customerInfo',
-			},
-		},
-		{ $sort: { progressDate: -1 } },
-	]);
-	//const taskList = await tasks.find({crmCode:crmData._id})
-	const columnOrder = crmData && crmData.crmSteps;
-	var showColumn = [];
-	var columns = {};
-	for (var i = 0; i < columnOrder.length; i++) {
-		const accessTemp = userAccess.find((item) => item.title === columnOrder[i].enTitle);
-		//console.log(access)
-		if (accessTemp || access > 8) {
-			columnOrder[i].access = access ? 'edit' : accessTemp.state;
-			showColumn.push(columnOrder[i]);
-			columns[columnOrder[i].enTitle] = [];
+		var myCreator = [];
+		if (access == 7) {
+			var userList = await user.find({ profile: { $in: userData.profile } }); //{StockId:userData.StockId})
+			myCreator = userList.map((item) => item._id.toString());
 		}
-	}
-	//console.log(columns)
-	const tasksToShow = [];
-	for (var c = 0; c < taskList.length; c++) {
-		if (taskList[c].result) {
-			var Number = taskList[c].result.Number;
-			var InvoiceID = taskList[c].result.InvoiceID;
-			var Message = taskList[c].result.Message;
-			taskList[c].result = { Number, InvoiceID, Message };
+		if (access == 3) {
+			myCreator = [userId];
 		}
-		var taskStep = taskList[c].taskStep;
-		var yesterday = new Date(Date.now() - 86400000); // that is: 24 * 60 * 60 * 1000
-		var taskDate = taskList[c].progressDate ? taskList[c].progressDate : taskList[c].date;
-		if (!taskList[c].progressDate) {
-			yesterday = new Date(Date.now() - 166400000);
+		var taskList = await tasks.aggregate([
+			//{$match:limitTask?{profile:limitTask}:{}},
+			{ $match: { crmId: crmId } },
+			{ $match: access < 8 ? { creator: { $in: myCreator } } : {} },
+			{
+				$addFields: {
+					user_Id: {
+						$convert: {
+							input: '$assign',
+							to: 'objectId',
+							onError: '',
+							onNull: '',
+						},
+					},
+				},
+			},
+			{
+				$lookup: {
+					from: 'users',
+					localField: 'user_Id',
+					foreignField: '_id',
+					as: 'userInfo',
+				},
+			},
+			{
+				$addFields: {
+					profile_Id: {
+						$convert: {
+							input: '$profile',
+							to: 'objectId',
+							onError: '',
+							onNull: '',
+						},
+					},
+				},
+			},
+			{
+				$lookup: {
+					from: 'profiles',
+					localField: 'profile_Id',
+					foreignField: '_id',
+					as: 'profileInfo',
+				},
+			},
+			{
+				$addFields: {
+					creator_Id: {
+						$convert: {
+							input: '$creator',
+							to: 'objectId',
+							onError: '',
+							onNull: '',
+						},
+					},
+				},
+			},
+			{
+				$lookup: {
+					from: 'users',
+					localField: 'creator_Id',
+					foreignField: '_id',
+					as: 'creatorInfo',
+				},
+			},
+			{
+				$addFields: {
+					customer_Id: {
+						$convert: {
+							input: '$customer',
+							to: 'objectId',
+							onError: '',
+							onNull: '',
+						},
+					},
+				},
+			},
+			{
+				$lookup: {
+					from: 'customers',
+					localField: 'customer_Id',
+					foreignField: '_id',
+					as: 'customerInfo',
+				},
+			},
+			{ $sort: { progressDate: -1 } },
+		]);
+		//const taskList = await tasks.find({crmCode:crmData._id})
+		const columnOrder = crmData && crmData.crmSteps;
+		var showColumn = [];
+		var columns = {};
+		for (var i = 0; i < columnOrder.length; i++) {
+			const accessTemp = userAccess.find((item) => item.title === columnOrder[i].enTitle);
+			//console.log(access)
+			if (accessTemp || access > 8) {
+				columnOrder[i].access = access ? 'edit' : accessTemp.state;
+				showColumn.push(columnOrder[i]);
+				columns[columnOrder[i].enTitle] = [];
+			}
 		}
-		if (taskStep == 'archive' || taskStep == 'cancel') if (taskDate < yesterday) continue;
-		try {
-			columns[taskStep].push(taskList[c]._id);
-			tasksToShow.push(taskList[c]);
-		} catch {}
-		//columnOrder.find(item=>item.enTitle===taskStep)
+		//console.log(columns)
+		const tasksToShow = [];
+		for (var c = 0; c < taskList.length; c++) {
+			if (taskList[c].result) {
+				var Number = taskList[c].result.Number;
+				var InvoiceID = taskList[c].result.InvoiceID;
+				var Message = taskList[c].result.Message;
+				taskList[c].result = { Number, InvoiceID, Message };
+			}
+			var taskStep = taskList[c].taskStep;
+			var yesterday = new Date(Date.now() - 86400000); // that is: 24 * 60 * 60 * 1000
+			var taskDate = taskList[c].progressDate ? taskList[c].progressDate : taskList[c].date;
+			if (!taskList[c].progressDate) {
+				yesterday = new Date(Date.now() - 166400000);
+			}
+			if (taskStep == 'archive' || taskStep == 'cancel') if (taskDate < yesterday) continue;
+			try {
+				columns[taskStep].push(taskList[c]._id);
+				tasksToShow.push(taskList[c]);
+			} catch {}
+			//columnOrder.find(item=>item.enTitle===taskStep)
+		}
+		return { userAccess, crmData: crmData, tasks: tasksToShow, crm: crmData, columnOrder: showColumn, columns: columns, myCreator };
+	} catch (error) {
+		return;
 	}
-	return { userAccess, crmData: crmData, tasks: tasksToShow, crm: crmData, columnOrder: showColumn, columns: columns, myCreator };
 };
 
 router.post('/update-tasks', auth, jsonParser, async (req, res) => {
